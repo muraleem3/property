ecdf7c783dd917a09b5a4e0156bcde5cf3d23fd3
Muralee Mohan <muraleem3@gmail.com>
2014-05-05 11:49:43 +0530

    pms

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+
+	def index
+	end
+
+  	def home_page
+  	end
+
+  	def contact_us
+  	end
+
+  	def property_results
+  	end
+
+  	def search_results
+  	end
+
+
+
+end
